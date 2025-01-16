a2b1a727821f09b89cf13e0685f9d5fc5cc10fff
sahargavriely <63425950+sahargavriely@users.noreply.github.com>
2025-01-16 16:26:25 +0200

    action (#8)

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 
 function main {
     source venv/bin/activate
-    venv/bin/python -m pytest --cov-report=html --cov-report=xml --cov-report=term --cov=flake8_kwargs_spaces tests/
+    venv/bin/python -m pytest --cov-branch --cov-report=html --cov-report=xml --cov-report=term --cov=flake8_kwargs_spaces tests/
     export TEST_RESULT=$?
     deactivate
     exit ${TEST_RESULT}
